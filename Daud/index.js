7390239cb61c6de0ae15cb1b9b36b9421cc575bf
Toufeeq02 <138937884+Toufeeq02@users.noreply.github.com>
2024-07-28 08:35:00 -0700

    Add files via upload

---
+++ Daud/index.js
@@ -0,0 +1,11 @@
+
+function openbutton() {
+    const menu = document.getElementsByClassName('menu_1')[0]; // Get the first element
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+  
+    } else {
+        menu.style.display = 'block';
+       
+    }
+}
